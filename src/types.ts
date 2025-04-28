bcdba7e7d90fbdcb474303da25b3b171670615bd
kildemal <bharathps37@gmail.com>
2025-04-28 05:34:08 +0000

    fix type issues

---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export interface Pokemon {
+    id: number;
+    name: string;
+    stats: number[];  // [HP, Attack, Defense, Special Attack, Special Defense, Speed]
+    types: number[];  // Type IDs (e.g., [12, 7])
+    abilities: number[][];  // Format: [abilityId, flag]
+    sprite?: string;  // Base64 encoded image
+  }
