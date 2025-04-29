9da79cd0b53107748f52b146fede2bff7163c4fd
Sp3cker <speker97@protonmail.com>
2025-04-29 16:21:14 -0500

    Remove Pixel Operator, better type badges

--- src/components/TypeBadge.tsx
+++ src/components/TypeBadge.tsx
@@ -2,22 +2,22 @@ import { getTypeColor, getTypeName, adjustColor } from "../utils/typeData";
 
 function TypeBadge({ typeId }: { typeId: number }) {
   // Type ID is passed as a prop
-  const typeColor = getTypeColor(typeId);
+  const [typeColor, endColor] = getTypeColor(typeId);
   const typeName = getTypeName(typeId);
 
-  // Set the background color and gradient
-  const typeStyle = {
-    backgroundColor: adjustColor(typeColor, -20),
-    // backgroundImage: `linear-gradient(105deg, ${typeColor} 50%, ${adjustColor(typeColor, -10)} 50%)`,
-  };
+    // Set the background color and gradient
+    const typeStyle = {
+      backgroundColor: adjustColor(typeColor, -20),
+      backgroundImage: `linear-gradient(0deg, ${typeColor}, ${endColor} 75%)`,
+    };
 
   return (
     <div
-      className="border-1 shadow-md/50 flex w-16 flex-shrink-0 items-center justify-center whitespace-nowrap rounded-sm border-gray-300 leading-tight text-white"
+      className="flex w-14 flex-shrink-0 items-center justify-center whitespace-nowrap rounded-sm"
       style={typeStyle}
     >
       {/*add type icons later */}
-      <p className="font-pixel pkmn-types flex self-center text-lg leading-5 md:text-xl md:leading-6">
+      <p className="pkmnem-face-short pkmn-types text-lg leading-5 md:text-xl md:leading-6">
         {typeName.toUpperCase()}
       </p>
     </div>
