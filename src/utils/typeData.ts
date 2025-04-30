44532062cc118f2966f6d1cd921f7346392d3bbb
Sp3cker <speker97@protonmail.com>
2025-04-29 22:49:43 -0500

    Fix shadows again

--- src/utils/typeData.ts
+++ src/utils/typeData.ts
@@ -3,26 +3,26 @@
 // https://raw.githubusercontent.com/iriv24/pokeemerald-expansion/refs/heads/master/graphics/interface/menu_info.png
 // https://www.spriters-resource.com/game_boy_advance/pokemonrubysapphire/sheet/64357/
 export const TYPE_INFO: {
-  [id: number]: { id: number; name: string; color: string, colorEnd: string };
+  [id: number]: { id: number; name: string; color: string; colorEnd: string };
 } = {
-  0: { id: 0, name: "Normal", color: "#A8A77A", colorEnd: "#ACAC7B" },
-  1: { id: 1, name: "Fighting", color: "#C22E28", colorEnd: "#C03028" },
-  2: { id: 2, name: "Flying", color: "#ACAC7B", colorEnd: "#9CDEDE" },
-  3: { id: 3, name: "Poison", color: "#A33EA1", colorEnd: "#FF5A8B" },
-  4: { id: 4, name: "Ground", color: "#BDA439",colorEnd: "#E0C068" },
-  5: { id: 5, name: "Rock", color: "#B6A136",colorEnd: "#BDA439" },
-  6: { id: 6, name: "Bug", color: "#808F13",colorEnd: "#A8B820" },
-  7: { id: 7, name: "Ghost", color: "#735797",colorEnd: "#705898" },
-  8: { id: 8, name: "Steel", color: "#8C8C9E",colorEnd: "#B8B8D0" },
-  10: { id: 10, name: "Fire", color: "#EE8130",colorEnd: "#F08030" },
-  11: { id: 11, name: "Water", color: "#6390F0",colorEnd: "#6890F0" },
-  12: { id: 12, name: "Grass", color: "#398b31", colorEnd: "#62A13D" },
-  13: { id: 13, name: "Electric", color: "#CF9A09",colorEnd: "#F8D030" },
-  14: { id: 14, name: "Psychic", color: "#F95587",colorEnd: "#F85888" },
-  15: { id: 15, name: "Ice", color: "#65A19E",colorEnd: "#98D8D8" },
-  16: { id: 16, name: "Dragon", color: "#6F35FC",colorEnd: "#7038F8" },
-  17: { id: 17, name: "Dark", color: "#705746",colorEnd: "#A040A0" },
-  23: { id: 23, name: "Fairy", color: "#D685AD",colorEnd: "#A040A0" },
+  0: { id: 0, name: "Normal", color: "#757553", colorEnd: "#a8a878" },
+  1: { id: 1, name: "Fighting", color: "#8d231d", colorEnd: "#c03028" },
+  2: { id: 2, name: "Flying", color: "#816bb2", colorEnd: "#a890f0" },
+  3: { id: 3, name: "Poison", color: "#6d2b6d", colorEnd: "#a040a0" },
+  4: { id: 4, name: "Ground", color: "#ad9450", colorEnd: "#e0c068" },
+  5: { id: 5, name: "Rock", color: "#847328", colorEnd: "#b8a038" },
+  6: { id: 6, name: "Bug", color: "#798417", colorEnd: "#a8b820" },
+  7: { id: 7, name: "Ghost", color: "#4a3a65", colorEnd: "#705898" },
+  8: { id: 8, name: "Steel", color: "#8a8a9d", colorEnd: "#b8b8d0" },
+  10: { id: 10, name: "Fire", color: "#bd6425", colorEnd: "#f08030" },
+  11: { id: 11, name: "Water", color: "#5171bd", colorEnd: "#6890f0" },
+  12: { id: 12, name: "Grass", color: "#59943b", colorEnd: "#78c850" },
+  13: { id: 13, name: "Electric", color: "#c4a526", colorEnd: "#f8d030" },
+  14: { id: 14, name: "Psychic", color: "#c4456c", colorEnd: "#f85888" },
+  15: { id: 15, name: "Ice", color: "#74a4a4", colorEnd: "#98d8d8" },
+  16: { id: 16, name: "Dragon", color: "#582cc4", colorEnd: "#7038f8" },
+  17: { id: 17, name: "Dark", color: "#3c2f27", colorEnd: "#705848" },
+  23: { id: 23, name: "Fairy", color: "#cc88cc", colorEnd: "#ffaaff" },
 };
 
 export function getTypeName(typeId: number): string {
