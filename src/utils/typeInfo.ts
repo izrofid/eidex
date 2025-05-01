139d93dd63b43ebc6666b13895f01b877f262a5f
kildemal <bharathps37@gmail.com>
2025-05-01 11:23:07 +0000

    feat: add type matchups
    - added function to find defensive matchups
    
    - added TypeMatchups component
    
    - added matchups to PokemonModal

--- src/utils/typeInfo.ts
+++ src/utils/typeInfo.ts
@@ -6,16 +6,55 @@
 import typeData from "../data/typeData.json";
 import { TypeData } from "../types";
 
-export const typeDataArray: TypeData[] = Object.values(typeData);
+export const typeDataArray: TypeData[] = [];
+Object.entries(typeData).forEach(([id, data]) => {
+  typeDataArray[Number(id)] = data;
+});
+
+const validTypes = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 23,
+];
 
 export function getTypeName(typeID: number): string {
   return typeDataArray[typeID]?.typeName || "Normal"; //Normal in case of fallback
 }
 
 export function getTypeColor(typeID: number): [string, string] {
-  const typeColor = [typeDataArray[typeID]?.color || "#A8A77A", typeDataArray[typeID]?.colorEnd || "#A8A878"];
+  const typeColor = [
+    typeDataArray[typeID]?.color || "#A8A77A",
+    typeDataArray[typeID]?.colorEnd || "#A8A878",
+  ];
 
   return typeColor as [string, string];
+}
+
+function codeToMult(code: number): number {
+  switch (code) {
+    case 20:
+      return 2;
+    case 5:
+      return 0.5;
+    case 1:
+      return 0;
+    case 0:
+      return 1;
+    default:
+      return 1;
+  }
+}
+
+export function getDefensiveMatchup(typePair: [number, number?]) {
+  const result: Record<number, number> = {};
+  for (const [atkID, typeData] of typeDataArray.entries()) {
+    if (!validTypes.includes(Number(atkID))) continue;
+    const { matchup } = typeData;
+    const multipliers = typePair
+      .filter((t): t is number => t !== undefined)
+      .map((t) => codeToMult(matchup[t]));
+    const total = multipliers.reduce((a, b) => a * b, 1);
+    result[atkID] = total;
+  }
+  return result;
 }
 
 export const adjustColor = (color: string, amount: number): string => {
