36c020363a8d4059ae3e744d11afabe8610822ee
kildemal <206095739+izrofid@users.noreply.github.com>
2025-05-16 14:26:41 +0000

    feat: add color to moves

--- src/components/PokemonModal/Learnset/MoveEntry.tsx
+++ src/components/PokemonModal/Learnset/MoveEntry.tsx
@@ -1,47 +1,62 @@
 import { useScreenWidth } from "../../../hooks/useScreenWidth";
 import { Move } from "../../../types";
 import { TypeBadge } from "../../TypeBadges/TypeBadge";
-import { getTypeSnapColor } from "@/utils/typeInfo";
+import { getTypeColor } from "@/utils/typeInfo";
 import MovePropBox from "./MovePropRow";
+import chroma from "chroma-js";
 
 type MoveEntryProps = {
   move: Move;
   level?: number;
 };
 
+const adjustedBgCache: Record<number, string> = {};
+
 const MoveEntry: React.FC<MoveEntryProps> = ({ move, level }) => {
   const screenWidth = useScreenWidth();
+
+  const typeId =move.type
+  let adjustedBg = adjustedBgCache[typeId];
+  if (!adjustedBg) {
+    const snapColor = getTypeColor(typeId)[1];
+    const bgColor = chroma(snapColor);
+    adjustedBg = bgColor.darken(1.2).mix("white", 0.05).alpha(0.12).css();
+    adjustedBgCache[typeId] = adjustedBg;
+  }
+
   return (
-    <div className="relative border-b-1 flex select-none flex-col gap-2 border-gray-500 bg-zinc-700/20 px-2 py-5">
+    <div className="border-b-1 relative flex select-none flex-col gap-2 border-gray-500 px-2 py-5"
+      style={{backgroundColor: adjustedBg}}>
       <div className="flex flex-col gap-2">
         <div className="flex items-center justify-between">
           <span>
             <span
-              className="text-base font-bold text-white"
-              style={{ color: getTypeSnapColor(move.type) }}
+              className="text-base font-bold"
+              style={{ color: getTypeColor(move.type)[0] }}
             >
               {move.name}
             </span>
-            <span className="font-semibold italic text-neutral-100/80">
+            <span className="text-sm" style={{color: getTypeColor(move.type)[0]}}>
               {level ? ` @ ${level}` : ""}
             </span>
           </span>
-
+          <span className="mr-3 h-6 w-6 object-contain">
+            <img src={`icons/category/${move.cat}.png`}></img>
+          </span>
           <TypeBadge typeId={move.type} screenWidth={screenWidth} />
         </div>
         <div className="flex justify-between">
           <div className="flex flex-row gap-5">
-            <span className="text-xs text-gray-400">
+            <span className="text-xs text-gray-100">
               Power: {move.power || "—"}
             </span>
-            <span className="text-xs text-gray-400">
+            <span className="text-xs text-gray-100">
               Acc: {move.acc || "—"}%
             </span>
-            <span className="absolute bottom-2 right-3"><MovePropBox move={move}/></span>
+            <span className="absolute bottom-2 right-3">
+              <MovePropBox move={move} />
+            </span>
           </div>
-          <span className="h-6 w-6 object-contain mr-3">
-            <img src={`icons/category/${move.cat}.png`}></img>
-          </span>
         </div>
       </div>
       <span className="mt-1 text-left text-xs font-normal italic text-gray-300">
