0eec6b309b043ed98b32a8d59dba944821a31e28
izrofid <izrofid@gmail.com>
2025-05-28 03:38:58 +0000

    Refactor: Remove TrainerIdDisplay and update CloseButton
    - Deleted `TrainerIdDisplay.tsx` as it is no longer needed.
    - Modified `CloseButton.tsx` to improve functionality and align with updated requirements.

--- src/components/CloseButton.tsx
+++ src/components/CloseButton.tsx
@@ -15,7 +15,7 @@ const CloseButton: FC<CloseButtonProps> = ({
 }) => {
   return (
     <Button
-      className={`text-gray-400 cursor-pointer hover:text-red-500 active:text-red-700 transition-all duration-200 focus:outline-none rounded-full p-1 hover:bg-zinc-700/30 leading-none ${className}`}
+      className={` ${className} text-gray-400 cursor-pointer hover:text-red-500 active:text-red-700 transition-all duration-200 focus:outline-none rounded-full p-1 hover:bg-zinc-700/30 leading-none`}
       onClick={onClick}
       aria-label="Close"
       type="button"
